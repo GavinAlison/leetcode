fa47a77666eb76bb98ce430bd7f4c22af3b41385
Gavin.Alison <921757697@qq.com>
2019-08-08 19:54:06 +0800

    fix

---
+++ algorithm/src/main/java/com/alison/QueueDemo.java
@@ -0,0 +1,71 @@
+package com.alison;
+
+import java.util.ArrayDeque;
+
+public class QueueDemo {
+
+    public int[] maxSlibingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0)
+            return new int[0];
+        int[] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[i]);
+            }
+            output[i] = max;
+        }
+        return output;
+    }
+
+    ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+    int[] nums;
+
+    public void clean_deque(int i, int k) {
+        // remove indexes of elements not from sliding window
+        if (!deq.isEmpty() && deq.getFirst() == i - k)
+            deq.removeFirst();
+
+        // remove from deq indexes of all elements
+        // which are smaller than current element nums[i]
+        while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) {
+            deq.removeLast();
+        }
+    }
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0)
+            return new int[0];
+        if (k == 1)
+            return nums;
+
+        // init deque and output
+        this.nums = nums;
+        int max_idx = 0;
+        for (int i = 0; i < k; i++) {
+            clean_deque(i, k);
+            deq.addLast(i);
+            // compute max in nums[:k]
+            if (nums[i] > nums[max_idx])
+                max_idx = i;
+        }
+        int[] output = new int[n - k + 1];
+        output[0] = nums[max_idx];
+
+        // build output
+        for (int i = k; i < n; i++) {
+            clean_deque(i, k);
+            deq.addLast(i);
+            output[i - k + 1] = nums[deq.getFirst()];
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        QueueDemo queueDemo = new QueueDemo();
+        queueDemo.maxSlibingWindow(nums, 3);
+    }
+}
--- algorithm/src/main/java/com/alison/StackDemo.java
+++ algorithm/src/main/java/com/alison/StackDemo.java
@@ -51,9 +51,81 @@ public class StackDemo {
         return maxans;
     }
 
+    public int longestValidParenthesses(String s) {
+        int maxlen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 2; j <= s.length(); j += 2) {
+                if (isValidTwo(s.substring(i, j))) {
+                    maxlen = Math.max(maxlen, j - i);
+                }
+            }
+        }
+        return maxlen;
+    }
+
+    public boolean isValidTwo(String s) {
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (!stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int longestValidParentheses3(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        String operation = "+-*/";
+        for (int i = 0; i < tokens.length; i++) {
+            if (!operation.contains(tokens[i])) {
+                stack.push(Integer.valueOf(tokens[i]));
+            } else {
+                int v1 = (Integer) stack.pop();
+                int v2 = (Integer) stack.pop();
+                if (tokens[i].equals("+")) {
+                    stack.push(v1 + v2);
+                }
+                if (tokens[i].equals("-")) {
+                    stack.push(v1 - v2);
+                }
+                if (tokens[i].equals("*")) {
+                    stack.push(v1 * v2);
+                }
+                if (tokens[i].equals("/")) {
+                    stack.push(v1 / v2);
+                }
+            }
+        }
+        return (Integer) stack.pop();
+    }
+
     public static void main(String[] args) {
         String s = "[][][][][]";
         StackDemo stackDemo = new StackDemo();
         System.out.println(stackDemo.isValid(s));
+        String[] arr = {"2","1","+","3","*"};
+        stackDemo.evalRPN(arr);
+
     }
 }
