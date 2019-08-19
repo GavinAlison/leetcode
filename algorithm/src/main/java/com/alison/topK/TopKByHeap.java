f51e21e0100d26122b3b9da40dcf70ce3b79f464
Gavin.Alison <921757697@qq.com>
2019-08-19 19:25:05 +0800

    fix

---
+++ algorithm/src/main/java/com/alison/topK/TopKByHeap.java
@@ -0,0 +1,29 @@
+package com.alison.topK;
+
+import java.util.PriorityQueue;
+
+public class TopKByHeap {
+
+    PriorityQueue<Integer> queue;
+    int maxHeapSize;
+
+    public void topk(int k, int[] nums) {
+        queue = new PriorityQueue<>(k);
+        maxHeapSize = k;
+        if (k < nums.length) {
+            for (int i = 0; i < k; i++) {
+                queue.add(nums[i]);
+            }
+            for (int j = k; j < nums.length; j++) {
+                if (nums[j] > queue.peek()) {
+                    queue.poll();
+                    queue.offer(nums[j]);
+                }
+            }
+        } else {
+            for (Integer integer : nums) {
+                queue.add(integer);
+            }
+        }
+    }
+}
